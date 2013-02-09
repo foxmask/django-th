8cd25f752f7495ec353b0b58f5656e9a73d4a1a9
Olivier Demah <olivier@foxmask.info>
2013-02-09 13:36:17 +0100

    indentation broken (tab vs space)  with scite , fixed with apatana

--- lib/feedsservice/feedsservice.py
+++ lib/feedsservice/feedsservice.py
@@ -6,19 +6,21 @@ __all__ = ['Feeds']
 
 class Feeds(object):
 
-	URL_TO_PARSE = ''	
+    URL_TO_PARSE = ''
 
-	def __init__(self, **kwargs):
-		
-		if 'url_to_parse' in kwargs and kwargs['url_to_parse'] != '':
-			self.URL_TO_PARSE = kwargs['url_to_parse']	
-		else:
-			raise Exception ('Missing argument "url_to_parse" eg. url_to_parse=\'/path/to/local/file.rss\' or url_to_parse=\'http://doamin.com/file.rss\'')
+    def __init__(self, **kwargs):
 
-	def datas(self):
-		'''
-			read the data from a given URL or path to a local file
-		'''
-		datas = feedparser.parse(self.URL_TO_PARSE)
-		for entry in datas.entries:
-			yield entry
+        if 'url_to_parse' in kwargs and kwargs['url_to_parse'] != '':
+            self.URL_TO_PARSE = kwargs['url_to_parse']
+        else:
+            raise Exception('Missing argument "url_to_parse" eg. \
+                            url_to_parse=\'/path/to/local/file.rss\' or \
+                            url_to_parse=\'http://doamin.com/file.rss\'')
+
+    def datas(self):
+        '''
+            read the data from a given URL or path to a local file
+        '''
+        datas = feedparser.parse(self.URL_TO_PARSE)
+        for entry in datas.entries:
+            yield entry
