b9d757bda278ff9953e399f98cd25fce967f15cb
Olivier Demah <olivier@foxmask.info>
2013-06-01 12:57:11 +0200

    v0.1.1

--- django_th/setup.py
+++ django_th/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='django_th',
-    version='0.1',
+    version='0.1.1',
     description='Django Trigger Happy acts when events occur and triggers actions for your account on all the social networks',
     author='Olivier Demah',
     author_email='olivier@foxmask.info',
     url='https://github.com/foxmask/django_th',
-    download_url="https://github.com/foxmask/django-th/archive/trigger-happy-0.1.zip",
+    download_url="https://github.com/foxmask/django-th/archive/trigger-happy-0.1.1.zip",
     packages=[],
     classifiers=[
         'Development Status :: 4 - Beta',
