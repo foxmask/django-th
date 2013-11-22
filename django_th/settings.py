77ea9bee5467a4809c0ffc29e7d2f183eeeee8a7
Olivier Demah <olivier@foxmask.info>
2013-11-22 23:44:07 +0100

    one check with unit test

--- django_th/settings.py
+++ django_th/settings.py
@@ -16,8 +16,8 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASES = {
     'default': {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': '',
-        'NAME': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': PROJECT_DIR + '/../trigger_happy.sqlite3',
         # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
