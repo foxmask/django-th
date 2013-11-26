68d1fb21c1e0457ed59a0304e6b216420399de68
Olivier Demah <olivier@foxmask.info>
2013-11-26 22:29:04 +0100

    update url with last django_debug_toolbar

--- django_th/urls.py
+++ django_th/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 from django_th.forms.wizard import DummyForm, ProviderForm, \
     ConsummerForm, ServicesDescriptionForm
+
 from django_th.views import TriggerListView, TriggerDeleteView, \
     TriggerUpdateView, \
     TriggerEditedTemplateView, TriggerDeletedTemplateView, \
@@ -14,94 +15,109 @@ from django_th.views import TriggerListView, TriggerDeleteView, \
     trigger_on_off, trigger_switch_all_to, \
     trigger_edit_provider, trigger_edit_consummer
 
-urlpatterns = patterns('',
-                       # ****************************************
-                       # admin module
-                       # ****************************************
-                       url(r'^admin/', include(admin.site.urls)),
-                       # ****************************************
-                       # auth module
-                       # ****************************************
-                       url(r'^auth/', include('django.contrib.auth.urls')),
-                       # ****************************************
-                       # customized lgout action
-                       # ****************************************
-                       url(r'^logout/$',
-                           'django_th.views.logout_view', name='logout'),
+urlpatterns = \
+    patterns('',
+    # ****************************************
+    # admin module
+    # ****************************************
+    url(r'^admin/', include(admin.site.urls)),
+    # ****************************************
+    # auth module
+    # ****************************************
+    url(r'^auth/', include('django.contrib.auth.urls')),
+    # ****************************************
+    # customized lgout action
+    # ****************************************
+    url(r'^logout/$',
+      'django_th.views.logout_view', name='logout'),
 
-                       # ****************************************
-                       # trigger happy module
-                       # ****************************************
-                       url(r'^$', TriggerListView.as_view(),
-                           name='base'),
-                       url(r'^trigger/$', TriggerListView.as_view(),
-                           name='home'),
+    # ****************************************
+    # trigger happy module
+    # ****************************************
+    url(r'^$', TriggerListView.as_view(),
+      name='base'),
+    url(r'^trigger/$', TriggerListView.as_view(),
+      name='home'),
+    # ****************************************
+    # * trigger
+    # ****************************************
+    url(r'^trigger/delete/(?P<pk>\d+)$',
+      TriggerDeleteView.as_view(),
+      name='delete_trigger'),
+    url(r'^trigger/edit/(?P<pk>\d+)$',
+      TriggerUpdateView.as_view(),
+      name='edit_trigger'),
+    url(r'^trigger/editprovider/(?P<trigger_id>\d+)$',
+      trigger_edit_provider,
+      name='edit_provider'),
+    url(r'^trigger/editconsummer/(?P<trigger_id>\d+)$',
+      trigger_edit_consummer,
+      name='edit_consummer'),
+    url(r'^trigger/edit/thanks',
+      TriggerEditedTemplateView.as_view()),
+    url(r'^trigger/delete/thanks',
+      TriggerDeletedTemplateView.as_view()),
+    url(r'^trigger/onoff/(?P<trigger_id>\d+)$',
+      trigger_on_off,
+      name="trigger_on_off"),
+    url(r'^trigger/all/(?P<switch>(on|off))$',
+      trigger_switch_all_to,
+      name="trigger_switch_all_to"),
+    # ****************************************
+    # * service
+    # ****************************************
+    url(r'^service/$', UserServiceListView.as_view(),
+      name='user_services'),
+    url(r'^service/add/$', UserServiceCreateView.as_view(),
+      name='add_service'),
+    url(r'^service/delete/(?P<pk>\d+)$',
+      UserServiceDeleteView.as_view(),
+      name='delete_service'),
+    url(r'^service/add/thanks',
+      UserServiceAddedTemplateView.as_view(),
+      name="service_added"),
+    url(r'^service/delete/$',
+      UserServiceDeleteView.as_view(),
+      name='delete_service'),
+    url(r'^service/delete/thanks',
+      UserServiceDeletedTemplateView.as_view()),
+    # ****************************************    
+    # wizard
+    # ****************************************
+    url(r'^service/create/$',
+      UserServiceWizard.as_view([ProviderForm,
+                                 DummyForm,
+                                 ConsummerForm,
+                                 DummyForm,
+                                 ServicesDescriptionForm]),
+      name='create_service'),
+    # every service will use django_th.views.finalcallback
+    # and give the service_name value to use to
+    # trigger the real callback
+    url(r"^callbackevernote/$",
+      "django_th.views.finalcallback",
+      {'service_name': 'ServiceEvernote', },
+      name="evernote_callback",
+      ),
+    
+    # dummy callback as a sample 
+    # url(r"^callbacktwitter/$",
+    #  "django_th.views.finalcallback",
+    #  {'service_name': 'ServiceTwitter', },
+    #  name="twitter_callback",
+    #  ),
 
-                       # ****************************************
-                       # * trigger
-                       # ****************************************
-                       url(r'^trigger/delete/(?P<pk>\d+)$',
-                           TriggerDeleteView.as_view(),
-                           name='delete_trigger'),
-                       url(r'^trigger/edit/(?P<pk>\d+)$',
-                           TriggerUpdateView.as_view(),
-                           name='edit_trigger'),
-                       url(r'^trigger/editprovider/(?P<trigger_id>\d+)$',
-                           trigger_edit_provider,
-                           name='edit_provider'),
-                       url(r'^trigger/editconsummer/(?P<trigger_id>\d+)$',
-                           trigger_edit_consummer,
-                           name='edit_consummer'),
-                       url(r'^trigger/edit/thanks',
-                           TriggerEditedTemplateView.as_view()),
-                       url(r'^trigger/delete/thanks',
-                           TriggerDeletedTemplateView.as_view()),
-                       url(r'^trigger/onoff/(?P<trigger_id>\d+)$',
-                           trigger_on_off,
-                           name="trigger_on_off"),
-                       url(r'^trigger/all/(?P<switch>(on|off))$',
-                           trigger_switch_all_to,
-                           name="trigger_switch_all_to"),
-                       # ****************************************
-                       # * service
-                       # ****************************************
-                       url(r'^service/$', UserServiceListView.as_view(),
-                           name='user_services'),
-                       url(r'^service/add/$', UserServiceCreateView.as_view(),
-                           name='add_service'),
-                       url(r'^service/delete/(?P<pk>\d+)$',
-                           UserServiceDeleteView.as_view(),
-                           name='delete_service'),
-                       url(r'^service/add/thanks',
-                           UserServiceAddedTemplateView.as_view(),
-                           name="service_added"),
-                       url(r'^service/delete/$',
-                           UserServiceDeleteView.as_view(),
-                           name='delete_service'),
-                       url(r'^service/delete/thanks',
-                           UserServiceDeletedTemplateView.as_view()),
+  # *********************************************
+  #  Linked Account
+  # *********************************************
+  # url(r"^linked_accounts/",
+  # include("linked_accounts.urls"))
+)
 
-                       # wizard
-                       url(r'^service/create/$',
-                           UserServiceWizard.as_view([ProviderForm,
-                                                      DummyForm,
-                                                      ConsummerForm,
-                                                      DummyForm,
-                                                      ServicesDescriptionForm]),
-                           name='create_service'),
-                       # every service will use django_th.views.finalcallback
-                       # and give the service_name value to use to
-                       # trigger the real callback
-                       url(r"^callbackevernote/$",
-                           "django_th.views.finalcallback",
-                           {'service_name': 'ServiceEvernote', },
-                           name="evernote_callback",
-                           ),
+from django.conf import settings
 
-
-                       # *********************************************
-                       #  Linked Account
-                       # *********************************************
-                       # url(r"^linked_accounts/",
-                       # include("linked_accounts.urls"))
-                       )
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
