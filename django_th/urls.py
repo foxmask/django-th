345e7f87278552e621a89cc451e38641835d9cf4
Olivier Demah <olivier@foxmask.info>
2013-12-01 01:55:21 +0100

    typo

--- django_th/urls.py
+++ django_th/urls.py
@@ -122,7 +122,6 @@ from django.conf import settings
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns +=
-        patterns('',
-                 url(r'^__debug__/', include(debug_toolbar.urls)),
-                 )
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
