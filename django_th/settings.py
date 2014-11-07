3c3028f91112c50f7d7e7ad334323a2829b1b634
Olivier Demah <olivier@foxmask.info>
2014-11-08 00:10:04 +0100

    Drop Site Framework fixed #74 fixed #71

--- django_th/settings.py
+++ django_th/settings.py
@@ -103,7 +103,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
