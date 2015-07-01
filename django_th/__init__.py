7bf64c02ae7cddeeb7fafc8865e9e85b88d52d1c
foxmask <olivier@foxmask.net>
2015-07-01 12:02:16 +0200

    upd tox travis __init__

--- django_th/__init__.py
+++ django_th/__init__.py
@@ -1,10 +1,4 @@
-from __future__ import absolute_import
-
 VERSION = (0, 10, 1)  # PEP 386
 __version__ = ".".join([str(x) for x in VERSION])
 
 default_app_config = 'django_th.apps.DjangoThConfig'
-
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
--- django_th/apps.py
+++ django_th/apps.py
@@ -1,5 +1,7 @@
+from __future__ import absolute_import
 from django.apps import AppConfig
 
+from .celery import app as celery_app
 
 class DjangoThConfig(AppConfig):
     name = 'django_th'
