1a66e3e72ccc81889f7a96dba9339da4ff749db2
Olivier Demah <olivier@foxmask.info>
2015-09-02 21:41:37 +0200

    reduced local_settings to only the sensible informations

--- django_th/local_settings_sample.py
+++ django_th/local_settings_sample.py
@@ -1,7 +1,3 @@
-from celery.schedules import crontab
-
-DEBUG = True
-
 TH_EVERNOTE = {
     'sandbox': True,
     'consumer_key': 'my key',
@@ -34,35 +30,4 @@ TH_GITHUB = {
     'password': 'password',
     'consumer_key': 'my key',
     'consumer_secret': 'my secret'
-}
-
-# CELERY
-BROKER_URL = 'redis://localhost:6379/0'
-
-CELERYBEAT_SCHEDULE = {
-    'read-data': {
-        'task': 'django_th.tasks.read_data',
-        'schedule': crontab(minute='12,24,36,48'),
-    },
-    'publish-data': {
-        'task': 'django_th.tasks.publish_data',
-        'schedule': crontab(minute='20,40,59'),
-    },
-    'outside-cache': {
-        'task': 'django_th.tasks.get_outside_cache',
-        'schedule': crontab(minute='15,30,45'),
-    },
-}
-
-
-# REDISBOARD
-REDISBOARD_DETAIL_FILTERS = ['.*']
-
-# needed to th_search and haystack
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
-    },
 }
