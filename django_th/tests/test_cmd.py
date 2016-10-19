2b85202eaf92a873024ae5beda54c1848d5470dd
FoxMaSk <foxmaskhome@gmail.com>
2016-10-19 19:03:35 +0200

    tox is kidding

--- django_th/tests/test_cmd.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-from django.core import management
-
-management.call_command('recycle', verbosity=0, interactive=False)
-management.call_command('read', verbosity=0, interactive=False)
-management.call_command('publish', verbosity=0, interactive=False)
