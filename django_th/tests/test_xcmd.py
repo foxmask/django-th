1119a2cccf1177725b88d08ecff1a691ba193981
FoxMaSk <foxmaskhome@gmail.com>
2016-10-19 19:08:26 +0200

    tox is kidding

--- django_th/tests/test_xcmd.py
+++ django_th/tests/test_xcmd.py
@@ -1,6 +1,13 @@
 # coding: utf-8
 from django.core import management
 
-management.call_command('recycle', verbosity=0, interactive=False)
-management.call_command('read', verbosity=0, interactive=False)
-management.call_command('publish', verbosity=0, interactive=False)
+
+from django.test import TestCase
+
+
+class TestCmdMgt(TestCase):
+
+    def test_run(self):
+        management.call_command('recycle', verbosity=0, interactive=False)
+        management.call_command('read', verbosity=0, interactive=False)
+        management.call_command('publish', verbosity=0, interactive=False)
