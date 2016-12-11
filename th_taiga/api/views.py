6b11d956cdc5ce4c62d9c51439f1a5500ea9a393
FoxMaSk <foxmaskhome@gmail.com>
2016-12-11 01:27:26 +0100

    factory pattern to simplify

--- th_taiga/api/views.py
+++ th_taiga/api/views.py
@@ -10,104 +10,232 @@ from django_th.services import default_provider
 from th_taiga.models import Taiga
 
 
-def epic(taiga_obj, action, data):
-    """
+class TaigaDomain:
 
-    :param taiga_obj: taiga object
-    :param action: craete/change/delete
-    :param data: data to return
-    :return: data
-    """
-    if action == 'create' and taiga_obj.notify_epic_create:
-        data['type_action'] = 'New Epic created'
-    elif action == 'change' and taiga_obj.notify_epic_change:
-        data['type_action'] = 'Changed Epic'
-    elif action == 'delete' and taiga_obj.notify_epic_delete:
-        data['type_action'] = 'Deleted Epic'
-    return data
+    def factory(type):
+        if type == "epic":
+            return Epic()
+        if type == "issue":
+            return Issue()
+        if type == "task":
+            return Task()
+        if type == "userstory":
+            return UserStory()
+        if type == "wikipage":
+            return WikiPage()
+        if type == "relateduserstory":
+            return RelatedUserStory()
+        assert 0, "Bad type creation: " + type
 
-
-def issue(taiga_obj, action, data):
-    """
-
-    :param taiga_obj: taiga object
-    :param action: craete/change/delete
-    :param data: data to return
-    :return: data
-    """
-    if action == 'create' and taiga_obj.notify_issue_create:
-        data['type_action'] = 'New Issue created'
-    elif action == 'change' and taiga_obj.notify_issue_change:
-        data['type_action'] = 'Changed Issue'
-    elif action == 'delete' and taiga_obj.notify_issue_delete:
-        data['type_action'] = 'Deleted Issue'
-    return data
+    factory = staticmethod(factory)
 
 
-def userstory(taiga_obj, action, data):
-    """
+class Epic(TaigaDomain):
 
-    :param taiga_obj: taiga object
-    :param action: craete/change/delete
-    :param data: data to return
-    :return: data
-    """
-    if action == 'create' and taiga_obj.notify_userstory_create:
-        data['type_action'] = 'New Userstory created'
-    elif action == 'change' and taiga_obj.notify_userstory_change:
-        data['type_action'] = 'Changed Userstory'
-    elif action == 'delete' and taiga_obj.notify_userstory_delete:
-        data['type_action'] = 'Deleted Userstory'
-    return data
+    def create(self, taiga_obj, data):
+        """
 
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_epic_create:
+            data['type_action'] = 'New Epic created'
+        return data
 
-def task(taiga_obj, action, data):
-    """
+    def change(self, taiga_obj, data):
+        """
 
-    :param taiga_obj: taiga object
-    :param action: craete/change/delete
-    :param data: data to return
-    :return: data
-    """
-    if action == 'create' and taiga_obj.notify_task_create:
-        data['type_action'] = 'New Task created'
-    elif action == 'change' and taiga_obj.notify_task_change:
-        data['type_action'] = 'Changed Task'
-    elif action == 'delete' and taiga_obj.notify_task_delete:
-        data['type_action'] = 'Deleted Task'
-    return data
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_epic_change:
+            data['type_action'] = 'Changed Epic'
+        return data
 
+    def delete(self, taiga_obj, data):
+        """
 
-def wikipage(taiga_obj, action, data):
-    """
-
-    :param taiga_obj: taiga object
-    :param action: craete/change/delete
-    :param data: data to return
-    :return: data
-    """
-    if action == 'create' and taiga_obj.notify_wikipage_create:
-        data['type_action'] = 'New Wikipage created'
-    elif action == 'change' and taiga_obj.notify_wikipage_change:
-        data['type_action'] = 'Changed Wikipage'
-    elif action == 'delete' and taiga_obj.notify_wikipage_delete:
-        data['type_action'] = 'Deleted Wikipage'
-    return data
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_epic_delete:
+            data['type_action'] = 'Deleted Epic'
+        return data
 
 
-def relateduserstory(taiga_obj, action, data):
-    """
+class Issue(TaigaDomain):
 
-    :param taiga_obj: taiga object
-    :param action: craete/change/delete
-    :param data: data to return
-    :return: data
-    """
-    if action == 'create' and taiga_obj.notify_relateduserstory_create:
-        data['type_action'] = 'New Related Userstory created'
-    elif action == 'delete' and taiga_obj.notify_relateduserstory_delete:
-        data['type_action'] = 'Deleted Related Userstory'
-    return data
+    def create(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param action: craete/change/delete
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_issue_create:
+            data['type_action'] = 'New Issue created'
+        return data
+
+    def change(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param action: craete/change/delete
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_issue_change:
+            data['type_action'] = 'Changed Issue'
+        return data
+
+    def delete(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param action: craete/change/delete
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_issue_delete:
+            data['type_action'] = 'Deleted Issue'
+        return data
+
+
+class UserStory(TaigaDomain):
+
+    def create(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_userstory_create:
+            data['type_action'] = 'New Userstory created'
+
+        return data
+
+    def change(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_userstory_change:
+            data['type_action'] = 'Changed Userstory'
+        return data
+
+    def delete(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_userstory_delete:
+            data['type_action'] = 'Deleted Userstory'
+        return data
+
+
+class Task(TaigaDomain):
+    def create(self,  taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_task_create:
+            data['type_action'] = 'New Task created'
+        return data
+
+    def change(self,  taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_task_change:
+            data['type_action'] = 'Changed Task'
+        return data
+
+    def delete(self,  taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_task_delete:
+            data['type_action'] = 'Deleted Task'
+        return data
+
+
+class WikiPage(TaigaDomain):
+
+    def create(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_wikipage_create:
+            data['type_action'] = 'New Wikipage created'
+        return data
+
+    def change(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_wikipage_change:
+            data['type_action'] = 'Changed Wikipage'
+        return data
+
+    def delete(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_wikipage_delete:
+            data['type_action'] = 'Deleted Wikipage'
+        return data
+
+
+class RelatedUserStory(TaigaDomain):
+
+    def create(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_relateduserstory_create:
+            data['type_action'] = 'New Related Userstory created'
+        return data
+
+    def delete(self, taiga_obj, data):
+        """
+
+        :param taiga_obj: taiga object
+        :param data: data to return
+        :return: data
+        """
+        if taiga_obj.notify_relateduserstory_delete:
+            data['type_action'] = 'Deleted Related Userstory'
+        return data
 
 
 def data_filter(trigger_id, **data):
@@ -123,21 +251,13 @@ def data_filter(trigger_id, **data):
     domain = data.get('type')
     data = data.get('data')
 
-    if domain == 'epic':
-        data = epic(taiga_obj, action, data)
-    elif domain == 'issue':
-        data = issue(taiga_obj, action, data)
-    elif domain == 'userstory':
-        data = userstory(taiga_obj, action, data)
-    elif domain == 'task':
-        data = task(taiga_obj, action, data)
-    elif domain == 'wikipage':
-        data = wikipage(taiga_obj, action, data)
-    elif domain == 'relateduserstory':
-        data = relateduserstory(taiga_obj, action, data)
-    else:
-        data = []
-
+    t = TaigaDomain.factory(domain)
+    if action == 'create':
+        t.create(taiga_obj, data)
+    elif action == 'change':
+        t.change(taiga_obj, data)
+    elif action == 'delete':
+        t.delete(taiga_obj, data)
     return data
 
 
