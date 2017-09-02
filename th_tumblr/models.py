34507adaece2296dbd491f7eacb2632268340a1c
FoxMaSk <foxmaskhome@gmail.com>
2017-09-02 22:27:34 +0200

    Tumblr Service integrated

---
+++ th_tumblr/models.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from django.db import models
+from django_th.models.services import Services
+
+
+class Tumblr(Services):
+
+    """
+        tumblr model to be adapted for the new service
+    """
+    blogname = models.CharField(max_length=80)
+    tag = models.CharField(max_length=80, blank=True)
+    trigger = models.ForeignKey('TriggerService')
+
+    class Meta:
+        app_label = 'django_th'
+        db_table = 'django_th_tumblr'
+
+    def __str__(self):
+        return self.blogname
+
+    def show(self):
+        return "My Tumblr %s" % self.blogname
