4cbce811350cf114b4d727a7f2bb41946a078cfd
Sinan Comert <sinancomert1@gmail.com>
2017-10-12 21:59:22 +0300

    Fix issue 199

--- django_th/admin.py
+++ django_th/admin.py
@@ -50,14 +50,31 @@ class ServicesManagedAdmin(admin.ModelAdmin):
         return super(ServicesManagedAdmin, self).get_form(request, obj,
                                                           **defaults)
 
+class NameListFilter(admin.SimpleListFilter):
+    title = 'name'
+    parameter_name = 'name'
+
+    def lookups(self, request, model_admin):
+        if request.GET.get('user__id__exact'):
+            user_set = set([s.name.name for s in UserService.objects
+                           .filter(user_id__exact=int(request.GET.get('user__id__exact')))])
+        else:
+            user_set = set([s.name.name for s in UserService.objects.all()])
+        return [(i, i) for i in user_set]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(name=self.value())
+        return queryset
+
 
 class UserServiceAdmin(admin.ModelAdmin):
-
     """
         get the list of the User Service
     """
     list_display = ('user', 'name', 'token')
-    list_filter = ['user', 'name']
+    list_filter = ['user', NameListFilter]
+
 
 
 class ProviderServiceListFilter(admin.SimpleListFilter):
