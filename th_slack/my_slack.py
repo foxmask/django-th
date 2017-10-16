b2eccf3fed8f92cd374f035b1e933b0093a12ea6
FoxMaSk <foxmask@users.noreply.github.com>
2017-10-16 08:10:26 +0200

    fixed : TypeError: must be str, not NoneType

--- th_slack/my_slack.py
+++ th_slack/my_slack.py
@@ -50,7 +50,7 @@ class ServiceSlack(ServicesMgr):
         title = self.set_title(data)
         if title is None:
             title = data.get('subject')
-        type_action = data.get('type_action')
+        type_action = data.get('type_action', '')
 
         # set the bot username of Slack to the name of the
         # provider service
